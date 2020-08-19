ecccc83b95cf3b1fc05e4b4de4a4aa12184ddfa2
Ayo-haha <69881651+Ayo-haha@users.noreply.github.com>
2020-08-19 11:23:33 +0800

    hello.cs

---
+++ hello.cs
@@ -0,0 +1,17 @@
+namespace hha
+{
+
+    class Program { 
+       public  static void Main(string[] mgs) {
+
+            int a = 10;
+            System.Console.WriteLine(a);
+
+
+        }
+
+
+    }
+
+
+}
